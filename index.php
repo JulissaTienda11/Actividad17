fa3790242a4982a4d974dd95dd21b44a8aa6c7bd
Julissa Tienda <160643018+JulissaTienda11@users.noreply.github.com>
2024-06-12 16:15:31 +0000

    Initial commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<html>
+  <head>
+    <title>PHP Test</title>
+  </head>
+  <body>
+    <?php 
+    echo '<p>Julissa Itzel Tienda Ramirez </p>';
+    echo '<p>Calle: 12 </p>'; 
+    echo '<p>Numero de casa: #1558-1</p>'; 
+    echo '<p>Colonia: Vista Hermosa </p>'; 
+    echo '<p>Municipio: Saltillo </p>';
+    echo '<p>Telefono: 8442110261 </p>'; 
+    ?> 
+
+</html>
